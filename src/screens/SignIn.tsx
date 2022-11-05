e218d8382cd50c31d906e5453bf1090bef42ac99
unknown <matheusbprossi@gmail.com>
2022-11-05 19:51:53 -0300

    first commit

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,10 @@
+import { Center, Text } from "native-base";
+import React from "react";
+
+export default function SignIn() {
+    return (
+        <Center flex={1} bgColor="gray.900">
+            <Text color="white" fontSize={24}>Sign In</Text>
+        </Center>
+    )
+}
